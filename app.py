5246c277a299e4902d8d3500c2ad0d71ddfd3d2b
Rigobert-bayili <basilebayili@gmail.com>
2026-03-17 06:45:03 +0000

    Ajout projets professionnels et GitHub

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 app.secret_key = "basile2026"
+
+GITHUB_USERNAME = "Rigobert-bayili"
 
 @app.route("/")
 def accueil():
@@ -13,10 +16,14 @@ def about():
 
 @app.route("/projets")
 def projets():
-    return render_template("projets.html")
+    url = f"https://api.github.com/users/{GITHUB_USERNAME}/repos?sort=updated&per_page=100"
+    response = requests.get(url)
+    repos = response.json()
+    return render_template("projets.html", repos=repos)
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
+    from flask import request, flash, redirect, url_for
     if request.method == "POST":
         nom = request.form["nom"]
         email = request.form["email"]
